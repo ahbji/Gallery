01123ca118faed20622ee57d9c98cfc6b32da9ff
andreaji <andrea.hb.ji@outlook.com>
2022-01-25 16:44:17 +0800

    添加 gallery footer

--- app/src/main/java/com/codingnight/example/gallery/GalleryAdapter.kt
+++ app/src/main/java/com/codingnight/example/gallery/GalleryAdapter.kt
@@ -11,6 +11,7 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -19,24 +20,53 @@ import com.bumptech.glide.request.target.Target
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerFrameLayout
 
-class GalleryAdapter : ListAdapter<PhotoItem, MyViewHolder>(DIFFCALLBACK) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val holder = MyViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.gallery_cell, parent, false)
-        )
-        holder.itemView.setOnClickListener {
-            Bundle().apply {
-                putParcelableArrayList("PHOTO_LIST", ArrayList(currentList))
-                putInt("PHOTO_POSITION",holder.adapterPosition)
-                holder.itemView.findNavController()
-                    .navigate(R.id.action_galleryFragment_to_pagerPhotoFragment, this)
+class GalleryAdapter : ListAdapter<PhotoItem, RecyclerView.ViewHolder>(DIFFCALLBACK) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val holder: RecyclerView.ViewHolder
+        if (viewType == NORMAL_VIEW_TYPE) {
+            holder = ItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.gallery_cell, parent, false)
+            )
+            holder.itemView.setOnClickListener {
+                Bundle().apply {
+                    putParcelableArrayList("PHOTO_LIST", ArrayList(currentList))
+                    putInt("PHOTO_POSITION", holder.adapterPosition)
+                    holder.itemView.findNavController()
+                        .navigate(R.id.action_galleryFragment_to_pagerPhotoFragment, this)
+                }
             }
+        } else {
+            holder = FooterViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.gallery_footer, parent, false).also {
+                    (it.layoutParams as StaggeredGridLayoutManager.LayoutParams).isFullSpan = true
+                }
+            )
         }
         return holder
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun getItemCount(): Int {
+        return super.getItemCount() + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == itemCount - 1)
+            FOOTER_VIEW_TYPE
+        else
+            NORMAL_VIEW_TYPE
+    }
+
+    companion object {
+        const val NORMAL_VIEW_TYPE = 0
+        const val FOOTER_VIEW_TYPE = 1
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (position == itemCount - 1) {
+            return
+        }
         val photoItem = getItem(position)
+        holder as ItemViewHolder
         with(holder) {
             imageView.layoutParams.height = photoItem.photoHeight
             textViewUser.text = photoItem.photoUser
@@ -88,20 +118,24 @@ class GalleryAdapter : ListAdapter<PhotoItem, MyViewHolder>(DIFFCALLBACK) {
 
     object DIFFCALLBACK : DiffUtil.ItemCallback<PhotoItem>() {
         override fun areItemsTheSame(oldItem: PhotoItem, newItem: PhotoItem): Boolean {
-            return oldItem === newItem
+            return oldItem.photoId == newItem.photoId
         }
 
         override fun areContentsTheSame(oldItem: PhotoItem, newItem: PhotoItem): Boolean {
-            return oldItem.photoId == newItem.photoId
+            // DataClass 重载 == 运算符实现了逐项比较
+            return oldItem == newItem
         }
     }
 }
 
 
-class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val shimmerViewCell: ShimmerFrameLayout = itemView.findViewById(R.id.shimmerViewCell)
     val imageView: ImageView = itemView.findViewById(R.id.imageView)
     val textViewUser: TextView = itemView.findViewById(R.id.textViewUser)
     val textViewLikes: TextView = itemView.findViewById(R.id.textViewLikes)
     val textViewCollections: TextView = itemView.findViewById(R.id.textViewCollections)
+}
+
+class FooterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 }
